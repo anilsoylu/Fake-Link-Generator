530624038cf71b8b0dbf98ab4ca5f685322c0289
Anıl Soylu <anil.soylu94@gmail.com>
2021-10-06 18:54:26 +0300

    application

---
+++ LinkUret/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace LinkUret
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.lblSonucYaz = new System.Windows.Forms.Label();
+            this.lblSiteAdi = new System.Windows.Forms.Label();
+            this.txtSiteAdi = new System.Windows.Forms.TextBox();
+            this.lblTrUrl = new System.Windows.Forms.Label();
+            this.lblTRLinkURL = new System.Windows.Forms.Label();
+            this.btnLink1 = new System.Windows.Forms.Button();
+            this.btnTRLinkCopy = new System.Windows.Forms.Button();
+            this.lblENLinkURL = new System.Windows.Forms.Label();
+            this.lblEnUrl = new System.Windows.Forms.Label();
+            this.btnEnLinkUret = new System.Windows.Forms.Button();
+            this.btnENLinkCopy = new System.Windows.Forms.Button();
+            this.lblCountText = new System.Windows.Forms.Label();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 249);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Padding = new System.Windows.Forms.Padding(0, 0, 2, 0);
+            this.toolStrip1.Size = new System.Drawing.Size(483, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(282, 22);
+            this.toolStripLabel1.Text = "Bu Program Anıl SOYLU Tarafından Geliştirilmiştir.";
+            // 
+            // lblSonucYaz
+            // 
+            this.lblSonucYaz.AutoSize = true;
+            this.lblSonucYaz.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonucYaz.Location = new System.Drawing.Point(6, 9);
+            this.lblSonucYaz.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSonucYaz.Name = "lblSonucYaz";
+            this.lblSonucYaz.Size = new System.Drawing.Size(13, 19);
+            this.lblSonucYaz.TabIndex = 3;
+            this.lblSonucYaz.Text = " ";
+            // 
+            // lblSiteAdi
+            // 
+            this.lblSiteAdi.AutoSize = true;
+            this.lblSiteAdi.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSiteAdi.ForeColor = System.Drawing.Color.Black;
+            this.lblSiteAdi.Location = new System.Drawing.Point(6, 54);
+            this.lblSiteAdi.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSiteAdi.Name = "lblSiteAdi";
+            this.lblSiteAdi.Size = new System.Drawing.Size(57, 15);
+            this.lblSiteAdi.TabIndex = 4;
+            this.lblSiteAdi.Text = "Site Adı : ";
+            // 
+            // txtSiteAdi
+            // 
+            this.txtSiteAdi.Location = new System.Drawing.Point(70, 46);
+            this.txtSiteAdi.Name = "txtSiteAdi";
+            this.txtSiteAdi.Size = new System.Drawing.Size(401, 29);
+            this.txtSiteAdi.TabIndex = 5;
+            // 
+            // lblTrUrl
+            // 
+            this.lblTrUrl.AutoSize = true;
+            this.lblTrUrl.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTrUrl.ForeColor = System.Drawing.Color.Brown;
+            this.lblTrUrl.Location = new System.Drawing.Point(6, 86);
+            this.lblTrUrl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTrUrl.Name = "lblTrUrl";
+            this.lblTrUrl.Size = new System.Drawing.Size(54, 15);
+            this.lblTrUrl.TabIndex = 6;
+            this.lblTrUrl.Text = "TR URL  :";
+            // 
+            // lblTRLinkURL
+            // 
+            this.lblTRLinkURL.AutoSize = true;
+            this.lblTRLinkURL.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTRLinkURL.ForeColor = System.Drawing.Color.Black;
+            this.lblTRLinkURL.Location = new System.Drawing.Point(67, 86);
+            this.lblTRLinkURL.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTRLinkURL.Name = "lblTRLinkURL";
+            this.lblTRLinkURL.Size = new System.Drawing.Size(10, 15);
+            this.lblTRLinkURL.TabIndex = 9;
+            this.lblTRLinkURL.Text = " ";
+            // 
+            // btnLink1
+            // 
+            this.btnLink1.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnLink1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btnLink1.Location = new System.Drawing.Point(9, 147);
+            this.btnLink1.Name = "btnLink1";
+            this.btnLink1.Size = new System.Drawing.Size(220, 34);
+            this.btnLink1.TabIndex = 12;
+            this.btnLink1.Text = "TR Link Üret";
+            this.btnLink1.UseVisualStyleBackColor = true;
+            this.btnLink1.Click += new System.EventHandler(this.BtnLink1_Click);
+            // 
+            // btnTRLinkCopy
+            // 
+            this.btnTRLinkCopy.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnTRLinkCopy.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btnTRLinkCopy.Location = new System.Drawing.Point(10, 201);
+            this.btnTRLinkCopy.Name = "btnTRLinkCopy";
+            this.btnTRLinkCopy.Size = new System.Drawing.Size(219, 34);
+            this.btnTRLinkCopy.TabIndex = 15;
+            this.btnTRLinkCopy.Text = "TR Linki Kopyala";
+            this.btnTRLinkCopy.UseVisualStyleBackColor = true;
+            this.btnTRLinkCopy.Click += new System.EventHandler(this.btnTRLinkCopy_Click);
+            // 
+            // lblENLinkURL
+            // 
+            this.lblENLinkURL.AutoSize = true;
+            this.lblENLinkURL.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblENLinkURL.ForeColor = System.Drawing.Color.Black;
+            this.lblENLinkURL.Location = new System.Drawing.Point(67, 115);
+            this.lblENLinkURL.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblENLinkURL.Name = "lblENLinkURL";
+            this.lblENLinkURL.Size = new System.Drawing.Size(10, 15);
+            this.lblENLinkURL.TabIndex = 17;
+            this.lblENLinkURL.Text = " ";
+            // 
+            // lblEnUrl
+            // 
+            this.lblEnUrl.AutoSize = true;
+            this.lblEnUrl.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblEnUrl.ForeColor = System.Drawing.Color.Brown;
+            this.lblEnUrl.Location = new System.Drawing.Point(7, 115);
+            this.lblEnUrl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEnUrl.Name = "lblEnUrl";
+            this.lblEnUrl.Size = new System.Drawing.Size(55, 15);
+            this.lblEnUrl.TabIndex = 16;
+            this.lblEnUrl.Text = "EN URL  :";
+            // 
+            // btnEnLinkUret
+            // 
+            this.btnEnLinkUret.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEnLinkUret.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btnEnLinkUret.Location = new System.Drawing.Point(251, 147);
+            this.btnEnLinkUret.Name = "btnEnLinkUret";
+            this.btnEnLinkUret.Size = new System.Drawing.Size(220, 34);
+            this.btnEnLinkUret.TabIndex = 18;
+            this.btnEnLinkUret.Text = "EN Link Üret";
+            this.btnEnLinkUret.UseVisualStyleBackColor = true;
+            this.btnEnLinkUret.Click += new System.EventHandler(this.BtnEnLinkUret_Click);
+            // 
+            // btnENLinkCopy
+            // 
+            this.btnENLinkCopy.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnENLinkCopy.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btnENLinkCopy.Location = new System.Drawing.Point(252, 201);
+            this.btnENLinkCopy.Name = "btnENLinkCopy";
+            this.btnENLinkCopy.Size = new System.Drawing.Size(219, 34);
+            this.btnENLinkCopy.TabIndex = 19;
+            this.btnENLinkCopy.Text = "EN Linki Kopyala";
+            this.btnENLinkCopy.UseVisualStyleBackColor = true;
+            this.btnENLinkCopy.Click += new System.EventHandler(this.BtnENLinkCopy_Click);
+            // 
+            // lblCountText
+            // 
+            this.lblCountText.AutoSize = true;
+            this.lblCountText.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblCountText.Location = new System.Drawing.Point(262, 9);
+            this.lblCountText.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCountText.Name = "lblCountText";
+            this.lblCountText.Size = new System.Drawing.Size(173, 19);
+            this.lblCountText.TabIndex = 20;
+            this.lblCountText.Text = " Toplam Kopyalanan Link :";
+            this.lblCountText.Visible = false;
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblCount.Location = new System.Drawing.Point(434, 9);
+            this.lblCount.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(21, 19);
+            this.lblCount.TabIndex = 21;
+            this.lblCount.Text = " 0";
+            this.lblCount.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(483, 274);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.lblCountText);
+            this.Controls.Add(this.btnENLinkCopy);
+            this.Controls.Add(this.btnEnLinkUret);
+            this.Controls.Add(this.lblENLinkURL);
+            this.Controls.Add(this.lblEnUrl);
+            this.Controls.Add(this.btnTRLinkCopy);
+            this.Controls.Add(this.btnLink1);
+            this.Controls.Add(this.lblTRLinkURL);
+            this.Controls.Add(this.lblTrUrl);
+            this.Controls.Add(this.txtSiteAdi);
+            this.Controls.Add(this.lblSiteAdi);
+            this.Controls.Add(this.lblSonucYaz);
+            this.Controls.Add(this.toolStrip1);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Link Üretme Aracı v1.0.1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.Label lblSonucYaz;
+        private System.Windows.Forms.Label lblSiteAdi;
+        private System.Windows.Forms.TextBox txtSiteAdi;
+        private System.Windows.Forms.Label lblTrUrl;
+        private System.Windows.Forms.Label lblTRLinkURL;
+        private System.Windows.Forms.Button btnLink1;
+        private System.Windows.Forms.Button btnTRLinkCopy;
+        private System.Windows.Forms.Label lblENLinkURL;
+        private System.Windows.Forms.Label lblEnUrl;
+        private System.Windows.Forms.Button btnEnLinkUret;
+        private System.Windows.Forms.Button btnENLinkCopy;
+        private System.Windows.Forms.Label lblCountText;
+        private System.Windows.Forms.Label lblCount;
+    }
+}
+
---
+++ LinkUret/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LinkUret
+{
+    public partial class Form1 : Form
+    {
+        int count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            count = Convert.ToInt32(lblCount.Text);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.ExitThread();
+        }
+
+        private void BtnLink1_Click(object sender, EventArgs e)
+        {
+            lblSonucYaz.Text = "";
+            btnTRLinkCopy.Enabled = true;
+            var link1list = File.ReadLines(@"trlink.txt", Encoding.GetEncoding("iso-8859-9"));
+            List<string> Yazilar = new List<string>();
+            foreach (var yenilink1 in link1list)
+            {
+                Yazilar.Add(yenilink1);
+            }
+
+            Random Rastgele = new Random();
+            int indeks = Rastgele.Next(1, Yazilar.Count);
+            lblTRLinkURL.Text = Yazilar[indeks];
+        }
+
+        private void BtnEnLinkUret_Click(object sender, EventArgs e)
+        {
+            lblSonucYaz.Text = "";
+            btnENLinkCopy.Enabled = true;
+            var link1list = File.ReadLines(@"enlink.txt", Encoding.GetEncoding("iso-8859-9"));
+            List<string> Yazilar = new List<string>();
+            foreach (var yenilink1 in link1list)
+            {
+                Yazilar.Add(yenilink1);
+            }
+
+            Random Rastgele = new Random();
+            int indeks = Rastgele.Next(0, Yazilar.Count);
+            lblENLinkURL.Text = Yazilar[indeks];
+        }
+
+        private void btnTRLinkCopy_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(lblTRLinkURL.Text.Trim()))
+            {
+                if (!string.IsNullOrEmpty(txtSiteAdi.Text.Trim()))
+                {
+                    lblCountText.Visible = true;
+                    lblCount.Visible = true;
+                    count = count + 1;
+                    lblCount.Text = count.ToString();
+                    Clipboard.SetText(txtSiteAdi.Text.Replace("/", "") + "/" + lblTRLinkURL.Text);
+                    lblSonucYaz.Text = "TR Link Başarıyla Kopyalandı !";
+                    lblSonucYaz.ForeColor = Color.Green;
+                    btnTRLinkCopy.Enabled = false;
+                }
+                else
+                {
+                    lblSonucYaz.Text = "Domain adı yazıp tekrar deneyin !";
+                    lblSonucYaz.ForeColor = Color.Brown;
+                }
+            }
+            else
+            {
+                lblSonucYaz.Text = "Kopyalamadan önce TR link oluşturun !";
+                lblSonucYaz.ForeColor = Color.Brown;
+            }
+        }
+
+        private void BtnENLinkCopy_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(lblENLinkURL.Text.Trim()))
+            {
+                if (!string.IsNullOrEmpty(txtSiteAdi.Text.Trim()))
+                {
+                    lblCountText.Visible = true;
+                    lblCount.Visible = true;
+                    count = count + 1;
+                    lblCount.Text = count.ToString();
+                    Clipboard.SetText(txtSiteAdi.Text.Replace("/", "") + "/" + lblENLinkURL.Text);
+                    lblSonucYaz.Text = "EN Link Başarıyla Kopyalandı !";
+                    lblSonucYaz.ForeColor = Color.Green;
+                    btnENLinkCopy.Enabled = false;
+                }
+                else
+                {
+                    lblSonucYaz.Text = "Domain adı yazıp tekrar deneyin !";
+                    lblSonucYaz.ForeColor = Color.Brown;
+                }
+            }
+            else
+            {
+                lblSonucYaz.Text = "Kopyalamadan önce EN link oluşturun !";
+                lblSonucYaz.ForeColor = Color.Brown;
+            }
+        }
+    }
+}
